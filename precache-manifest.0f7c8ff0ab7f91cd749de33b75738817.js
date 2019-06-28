2d927e74d4b57783d068cd64a2c3e55ac0e7136a
avdotion <avdotion@yandex.com>
2019-06-28 22:29:54 +0300

    Updates

---
+++ precache-manifest.0f7c8ff0ab7f91cd749de33b75738817.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4ce0539f6e205ef02d400d435b1364",
+    "url": "/clue/index.html"
+  },
+  {
+    "revision": "f0564fca4af00018f8a5",
+    "url": "/clue/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f0ef42fb908edbb8b51d",
+    "url": "/clue/static/css/main.903a9919.chunk.css"
+  },
+  {
+    "revision": "f0564fca4af00018f8a5",
+    "url": "/clue/static/js/2.7735fd30.chunk.js"
+  },
+  {
+    "revision": "f0ef42fb908edbb8b51d",
+    "url": "/clue/static/js/main.572102f7.chunk.js"
+  },
+  {
+    "revision": "3595e73f01376b044b9a",
+    "url": "/clue/static/js/runtime~main.6f66459e.js"
+  },
+  {
+    "revision": "fd1a551bae73ccce7e960227095a18b7",
+    "url": "/clue/static/media/lock_emoji.fd1a551b.png"
+  }
+]);
