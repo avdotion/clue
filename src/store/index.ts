ad72c557b58688099201771c2e07119fea219cac
Danila Avdoshin <avdotion@yandex.com>
2020-02-22 23:28:32 +0300

    Притащить RxJS и redux-observable в Pitaya (#30)
    * #29: добавил rxjs и redux-observable в зависимости
    
    * #29: подключил rootEpic
    
    В качестве бонуса обновил описание типов по Piotrek Witek

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,36 @@
-import {createStore} from 'redux';
-import {StateType} from 'typesafe-actions';
+import {createStore, applyMiddleware, compose} from 'redux';
+import {createEpicMiddleware, Epic} from 'redux-observable';
+import {StateType, ActionType} from 'typesafe-actions';
 
 import rootReducer from './rootReducer';
+import rootAction from './rootAction';
+import rootEpic from './rootEpic';
 
 export type RootState = StateType<ReturnType<typeof rootReducer>>;
+export type RootAction = ActionType<typeof rootAction>;
+export type RootEpic = Epic<RootAction, RootAction, RootState>;
+
+export const epicMiddleware = createEpicMiddleware();
+
+const middlewares = [
+  epicMiddleware,
+];
+
+const composeEnhancers =
+  (process.env.NODE_ENV === 'development' &&
+  (window as any) &&
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
+  || compose;
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
 
 const initialState = {};
 
 const store = createStore(
   rootReducer,
   initialState,
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  enhancer
 );
+
+epicMiddleware.run(rootEpic);
 
 export default store;
