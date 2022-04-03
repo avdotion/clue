ca4a26510b88d0e87940661db1a0441ec746bba9
Danila Avdoshin <avdotion@yandex.com>
2022-04-03 11:55:45 +0300

    Updates

---
+++ precache-manifest.5427bc21c874e441d2d9ab557936c103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8bc331d981f7e63b92757a8fb3c1c14",
+    "url": "/clue/index.html"
+  },
+  {
+    "revision": "8732765430ceda3c0546",
+    "url": "/clue/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8732765430ceda3c0546",
+    "url": "/clue/static/js/2.e7ac8628.chunk.js"
+  },
+  {
+    "revision": "f9b2260005236643be36c7a883c21604",
+    "url": "/clue/static/js/2.e7ac8628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f4c01b6a6a62e19c67",
+    "url": "/clue/static/js/main.857f84ba.chunk.js"
+  },
+  {
+    "revision": "62ecee1c0699ce77b2d8",
+    "url": "/clue/static/js/runtime-main.a9247624.js"
+  }
+]);
